6ebaef6b16d4f9e32b6f84c8e21a32cb6bff4cc4
David Bujaldon Escudero <davi_elx@hotmail.com>
2022-10-02 13:16:04 +0200

    probando

--- Modulo_4_Extra/app.js
+++ Modulo_4_Extra/app.js
@@ -90,11 +90,14 @@ document.getElementById("buttonCalcular").addEventListener("click", resultadoIgu
 
 // La tarifa de la habitación por noche es de 100€ para categoría Standard, 120€ Junior Suite y 150€ Suite.
 // El uso del SPA incrementa la tarifa anterior en 20€.
-// Una vez tengas la tarifa en función de la categoría y el SPA, la ocupación (individual o triple) actúa como un factor de decremento o incremento, siendo la opción de doble el estándar. Es decir, el uso individual reduce un 25% la tarifa por noche, mientras que el uso triple la incrementa en un 25%. El uso doble la deja tal cual.
+// Una vez tengas la tarifa en función de la categoría y el SPA, la ocupación (individual o triple) actúa como un factor 
+// de decremento o incremento, siendo la opción de doble el estándar. Es decir, el uso individual reduce un 25% la tarifa por noche,
+// mientras que el uso triple la incrementa en un 25%. El uso doble la deja tal cual.
 // Cada noche de parking suma 10€.
 // Un ejemplo de estancia sería:
 
-// 5 noches x Hab. Estándar con SPA de uso individual + 2 noches de parking. En este caso la tarifa por noche sería: (100€ noche + 20€ spa) al 75% por uso individual = 90€/noche x 5 noches = 450€ + (10€ parking x 2 noches) = 470€ TOTAL.
+// 5 noches x Hab. Estándar con SPA de uso individual + 2 noches de parking. En este caso la tarifa por noche sería:
+// (100€ noche + 20€ spa) al 75% por uso individual = 90€/noche x 5 noches = 450€ + (10€ parking x 2 noches) = 470€ TOTAL.
 
 // OPCIONAL
 
@@ -107,15 +110,47 @@ document.getElementById("buttonCalcular").addEventListener("click", resultadoIgu
         habitaciónJuniorSuite : 120,
         habitaciónSuite : 150
     };
-    var spa;
-    var ocupaciónIndividual, ocupaciónDoble, ocupaciónTriple;
-    var numeroNoches;
-    var numeroParking;
+    var spa = 20;
     var precioNocheParking = 10;
+    var ocupaciónIndividual = (habitacion * (0,75));
+    var ocupaciónDoble = habitacion;
+    var ocupaciónTriple = (habitacion * 1,25);
+    var numeroNoches = document.getElementById('noches').value
+    var numeroNochesParking = document.getElementById('nochesParking').value
     var calcular;
-    var tarifaHabitacionNoche = 100;
+    var habitacion;
+    var ocupacion;
+    var resultado;
+    
 
-//operaciones
+    function habitacionElegida(){
+        var selectHabitacion = document.getElementById('selectHabitacion')
+        habitacion = selectHabitacion.value;
+    }
+
+    function ocupacionElegida(){
+        var selectOcupacion = document.getElementById('selectOcupacion')
+        ocupacion = selectOcupacion.value;
+    }
+    function spaElegido(){
+        if(document.getElementById('spaElegido').checked){
+            habitacion = habitacion + 20;
+        }else{
+            habitacion = habitacion;
+        }
+    }
+        
+    function resultado(){
+        resultado = (ocupaciónIndividual * numeroNoches) + (10 * numeroNochesParking);
+        document.getElementById('resultado').value = resultado;
+    }
+
+    document.getElementById("Calcular").addEventListener("click", resultado);
+    
+// 5 noches x Hab. Estándar con SPA de uso individual + 2 noches de parking. En este caso la tarifa por noche sería:
+// (100€ noche + 20€ spa) al 75% por uso individual = 90€/noche x 5 noches = 450€ + (10€ parking x 2 noches) = 470€ TOTAL.
+
+
 
     
 
